2c96f94fe55c930dfc5a4df6ee109e9f7be8ead8
lavanyasanka <lbillakanti@gmail.com>
2017-05-25 14:00:47 -0500

    refined CRUD example

--- src/edu/learn/jdbc/Dao/implementation/Daycare_requestDaoImpl.java
+++ src/edu/learn/jdbc/Dao/implementation/Daycare_requestDaoImpl.java
@@ -1,6 +1,7 @@
 package edu.learn.jdbc.Dao.implementation;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -27,20 +28,21 @@ public class Daycare_requestDaoImpl extends JdbcUtility implements Daycare_reque
 		public List<Daycare_request> getAllRequests() {
 		
 		try {
+			Daycare_request dr = new Daycare_request();
 			List<Daycare_request> list = new ArrayList<Daycare_request>();
 			Connection con = getConnection();
 			
 			if(con != null){
 				System.out.println("Connection is succesfull");
 			}
-			Statement stmt = con.createStatement();
 			String sql = "SELECT ID_DAYCARE_REQUEST, DT_LAST_UPDATE, ID_LAST_UPDATE_PERSON, DT_CREATED, ID_CREATED_PERSON, ID_CASE, ID_STAGE, ID_EVENT, IND_INVALID, DT_INVALID, TXT_COMMENT_INVALID " +
-			"FROM DAYCARE_REQUEST WHERE ID_CREATED_PERSON = 24813632";
+					"FROM DAYCARE_REQUEST WHERE ID_CREATED_PERSON = 24813632"; //+dr.getID_CREATED_PERSON(); //24813632
 			
+			//PreparedStatement stmt = con.prepareStatement(sql);
+			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(sql);
 			while (rs.next())
 			{
-				Daycare_request dr = new Daycare_request();
 				dr.setID_DAYCARE_REQUEST(rs.getInt("ID_DAYCARE_REQUEST"));
 				dr.setDT_LAST_UPDATE(rs.getDate("DT_LAST_UPDATE"));
 				dr.setID_LAST_UPDATE_PERSON(rs.getInt("ID_LAST_UPDATE_PERSON"));
@@ -105,19 +107,15 @@ public class Daycare_requestDaoImpl extends JdbcUtility implements Daycare_reque
 	@Override
 	public void updateDaycare_request(Daycare_request daycareRequest) {
 		PreparedStatement stmt = null;
-	  //  ArrayList list = new ArrayList();
 		try {
 			Connection con = getConnection();
-			String sql = " UPDATE DAYCARE_REQUEST SET ID_LAST_UPDATE_PERSON =" +daycareRequest.getID_LAST_UPDATE_PERSON() +" WHERE ID_DAYCARE_REQUEST = "+ daycareRequest.getID_DAYCARE_REQUEST();
-			
+			//String sql = " UPDATE DAYCARE_REQUEST SET ID_LAST_UPDATE_PERSON =" +daycareRequest.getID_LAST_UPDATE_PERSON() +" WHERE ID_DAYCARE_REQUEST = "+ daycareRequest.getID_DAYCARE_REQUEST();
+			String sql = " UPDATE DAYCARE_REQUEST SET ID_LAST_UPDATE_PERSON =? WHERE ID_DAYCARE_REQUEST = ?";
 			stmt = con.prepareStatement(sql);
-			
-			//stmt.setInt(1, daycareRequest.getID_LAST_UPDATE_PERSON());
-			//stmt.setInt(2, daycareRequest.getID_DAYCARE_REQUEST());
-				        
-			int count= stmt.executeUpdate(sql);
-		
-			//System.out.println(count +"Rows affected.......");
+			stmt.setInt(1, daycareRequest.getID_LAST_UPDATE_PERSON());
+			stmt.setInt(2, daycareRequest.getID_DAYCARE_REQUEST());
+			int count = stmt.executeUpdate();	        
+				System.out.println(count +"Rows affected.......");
 			con.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -129,8 +127,45 @@ public class Daycare_requestDaoImpl extends JdbcUtility implements Daycare_reque
 
 	@Override
 	public void deleteDaycare_request(Daycare_request daycareRequest) {
-		// TODO Auto-generated method stub
+
+		try {
+			Connection con = getConnection();
+			String sql = "DELETE FROM DAYCARE_REQUEST WHERE ID_DAYCARE_REQUEST = ?";
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, daycareRequest.getID_DAYCARE_REQUEST() );
+			int count = pstmt.executeUpdate();
+			System.out.println(count + "rows got deleted ");
+			con.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+	}
+
+	@Override
+	public void insertDaycare_request(Daycare_request daycareRequest) {
+		try {
+			Connection con = getConnection();
+			String sql = "INSERT INTO DAYCARE_REQUEST ( ID_LAST_UPDATE_PERSON, DT_CREATED, ID_CREATED_PERSON, ID_CASE, ID_STAGE, ID_EVENT, IND_INVALID, DT_INVALID, TXT_COMMENT_INVALID)"+
+			"VALUES (? , ? , ? , ?, ? , ? , ?, ?, ? )";
+			PreparedStatement psmt = con.prepareStatement(sql);
+			psmt.setInt(1, daycareRequest.getID_LAST_UPDATE_PERSON());
+			psmt.setDate(2, (Date) daycareRequest.getDT_CREATED());
+			psmt.setInt(3, daycareRequest.getID_CREATED_PERSON());
+			psmt.setInt(4, daycareRequest.getID_CASE());
+			psmt.setInt(5, daycareRequest.getID_STAGE());
+			psmt.setInt(6, daycareRequest.getID_EVENT());
+			psmt.setString(7, daycareRequest.getIND_INVALID());
+			psmt.setDate(8, (Date) daycareRequest.getDT_INVALID());
+			psmt.setString(9, daycareRequest.getTXT_COMMENT_INVALID());
+			int count = psmt.executeUpdate();
+			System.out.println(count + "rows inserted.....");
+			con.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 
--- src/edu/learn/jdbc/Dao/test/TestDaycareRequest.java
+++ src/edu/learn/jdbc/Dao/test/TestDaycareRequest.java
@@ -9,19 +9,45 @@ import edu.learn.jdbc.Dao.model.Daycare_request;
 public class TestDaycareRequest {
 	public static void main(String[] args) throws Exception{
 		Daycare_requestDaoImpl r1 = new Daycare_requestDaoImpl();
-		//List<Daycare_request> list = r1.getAllRequests();
-		//list.forEach(ls -> System.out.println(ls));
-		//Daycare_request dayr = r1.getRequest(1189924);
-			//System.out.println(dayr);
-		 Daycare_request req1 = new Daycare_request();
-		 req1.setID_DAYCARE_REQUEST(1189922);
-		 req1.setID_LAST_UPDATE_PERSON(24813632);
-		//Daycare_request req2 = new Daycare_request(1189922);
+/*		
+		//***************SELECT - all records*******************
+		Daycare_request req1 = new Daycare_request();
+		req1.setID_CREATED_PERSON(24813632);
+		List<Daycare_request> list = r1.getAllRequests();
+		list.forEach(ls -> System.out.println(ls));
+		*/
+		
+		//***************SELECT - one record*******************		
+		
+/*		Daycare_request dayr = r1.getRequest(1189881);
+			System.out.println(dayr);*/
+			
+			
+	//**************************update ****************
+/*		Daycare_request req = new Daycare_request(1189922);
+		req.setID_DAYCARE_REQUEST(1189922);
+		req.setID_LAST_UPDATE_PERSON(2271);//24813632
+	    r1.updateDaycare_request(req1);
+*/
+	
+/*		// ***********for delete******************
+		Daycare_request req1 = new Daycare_request(1189939);//1189939
+		System.out.println("rows"+ req1);
+		 r1.deleteDaycare_request(req1);
+*/
 		 
-		 //r1.updateDaycare_request(req1);
-		// System.out.println(req1);
-		r1.updateDaycare_request(req1);
-		System.out.println("after update"+ req1);
+/*		 //*********************for insert******************
+		 Daycare_request req1 = new Daycare_request();
+		// req1.setDT_CREATED(todate('25/05/2017', 'DD/MM/YYYY'));
+		 req1.setID_LAST_UPDATE_PERSON(24813632);
+		 req1.setID_CREATED_PERSON(24813632);
+		 req1.setID_CASE(25852854);
+		 req1.setID_STAGE(68326964);
+		 req1.setID_EVENT(266660193);
+		 req1.setIND_INVALID("Y");
+		 req1.setDT_INVALID(null);
+		 req1.setTXT_COMMENT_INVALID("something something something");
+		 r1.insertDaycare_request(req1);*/
 	}
 
 }
